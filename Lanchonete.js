d4b72d6229e031f482e35222008a430898acbbb9
Lucas Grilo de Noronha <grilomaker@gmail.com>
2025-04-17 21:42:41 -0300

    novo

---
+++ Lanchonete.js
@@ -0,0 +1,24 @@
+let arma = prompt('Escreva uma arma');
+let motivo = prompt('Escreva um motivo');
+
+if (arma == "Faca" && motivo == "Vingança"){
+    alert("O Assassina é o João");
+} else if(arma != "Faca" || motivo != "Vingança"){
+    alert("Não existe relação entre a arma e o motivo.")
+}
+
+if (arma == "Veneno" && motivo == "Dinheiro"){
+    alert("A Assassina é a Maria");
+} else if(arma != "Veneno" || motivo != "Dinheiro"){
+    alert("Não existe relação entre a arma e o motivo.")
+}
+
+if (arma == "Pistola" && motivo == "Ciúmes"){
+    alert("O Assassina é o Pedro");
+} else if(arma != "Pistola" || motivo != "Ciúmes"){
+    alert("Não existe relação entre a arma e o motivo.")
+}
+
+
+
+
